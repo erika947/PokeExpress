25432117bd98dbf8a9022f9c6d554e4128dca205
Erika Rivera <rivera.erika07@gmail.com>
2022-07-28 14:01:40 -0400

    My server is set up and running

---
+++ pokemon_app/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 3003;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Pokemon App!')
+})
+
+app.listen(port, () => {
+    console.log(`*** Listening on http://localhost:${port} ***`)
+})    
