69bec9f09de26c8ecbfc12c56825ea92ad259b06
Erika Rivera <rivera.erika07@gmail.com>
2022-08-04 10:35:34 -0400

    Correcting Pokemon to pokemon

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3003;
 
-const Pokemon = require('./models/Pokemon')
+const Pokemon = require('./models/pokemon')
 mongoose.connect(process.env.MONGO_URI);
 mongoose.connection.once("open", () => {
     console.log("connected to mongo");
